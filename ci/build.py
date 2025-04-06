5091a00ffd7462bef03b752d964f285aef8b5034
Dean Stevens <deanstevens823@gmail.com>
2025-04-06 15:00:11 +1000

    Add basic CI workflow

---
+++ ci/build.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import typing as t;
+
+import common;
+from common import run, done;
+
+def build_features(features: t.Iterable[str]):
+    run(f'cargo build --features "{",".join(features)}"')
+
+if __name__ == "__main__":
+    print("Clean", flush=True)
+    run("cargo clean")
+
+    print()
+    print("Build - No features", flush=True)
+    run("cargo build")
+
+    print()
+    print("Build - All features", flush=True)
+    run("cargo build --all-features")
+
+    print()
+    print("Build - Feature permutations (parallel)", flush=True)
+    common.permute_features_parallel(build_features)
+
+    done()
---
+++ ci/common.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+features = {"mutex", "rwlock", "std"}
+
+import os;
+import itertools;
+import threading;
+import typing as t;
+
+def permute_features_parallel(target: t.Callable[[t.Iterable[str]], None]):
+    features_permuted = []
+    for i in range(1, len(features) + 1):
+        features_permuted.extend(itertools.combinations(features, i))
+
+    threads: t.List[threading.Thread] = []
+    for selected_features in features_permuted:
+        thread = threading.Thread(
+            target = target, 
+            args = (selected_features, )
+        )
+        thread.start()
+        threads.append(thread)
+    
+    for thread in threads:
+        thread.join()
+
+
+script_error = threading.Event()
+def run(command: str) -> bool:
+    global script_error
+    if os.system(command) != 0:
+        script_error.set()
+        return False
+    else:
+        return True
+
+
+def done():
+    global script_error
+    exit(1 if script_error.is_set() else 0)
