1f99eb2e651b1682f617bc81170bab93760c4537
Dean Stevens <deanstevens823@gmail.com>
2025-04-08 06:37:49 +1000

    Add `--release` mode test to `build` check

--- ci/build.py
+++ ci/build.py
@@ -6,6 +6,7 @@ from common import run, done;
 
 def build_features(features: t.Iterable[str]):
     run(f'cargo build --features "{",".join(features)}"')
+    run(f'cargo build --features "{",".join(features)}" --release')
 
 if __name__ == "__main__":
     print("Clean", flush=True)
@@ -14,10 +15,12 @@ if __name__ == "__main__":
     print()
     print("Build - No features", flush=True)
     run("cargo build")
+    run("cargo build --release")
 
     print()
     print("Build - All features", flush=True)
     run("cargo build --all-features")
+    run("cargo build --all-features --release")
 
     print()
     print("Build - Feature permutations (parallel)", flush=True)
